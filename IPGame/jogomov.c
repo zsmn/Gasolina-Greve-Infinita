bf998d9aab2e936af9b0d5ef69a2789678a61592
Zilde Neto <37539515+zsmn@users.noreply.github.com>
2018-06-05 20:44:56 -0300

    Update jogomov.c
    added menu 
    added character selection

--- IPGame/jogomov.c
+++ IPGame/jogomov.c
@@ -4,6 +4,8 @@
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro_primitives.h>
 #include <stdio.h>
+#include <stdbool.h>
+const float tempofade = 1.5;
 const int LARGURA_TELA = 960;
 const int ALTURA_TELA = 703; //ðeclaro o tamanho das telas
 const int passo = 3;  //declaro quantos passos ando
@@ -11,9 +13,23 @@ ALLEGRO_DISPLAY *janela = NULL; //ponteiro para a janela
 ALLEGRO_AUDIO_STREAM *musica = NULL; //ponteiro para a musica
 ALLEGRO_EVENT_QUEUE *fila_eventos = NULL;  //ponteiro para a fila de eventos
 ALLEGRO_BITMAP *imagem = NULL;  //ponteiro para a imagem
+ALLEGRO_BITMAP *menu = NULL;  //ponteiro para o menu do jogo
 ALLEGRO_BITMAP *grupo = NULL;   //lonteiro para o bitmap da imagem do grupo
 ALLEGRO_BITMAP *fundo = NULL; //ponteiro do pano de fundo
 ALLEGRO_BITMAP *quadrado = NULL;  //quadradinho q eh o jogador
+
+/* tela de seleção de personagens */
+ALLEGRO_BITMAP *personagens = NULL; // tela da seleção de personagens
+
+/* personagens */
+ALLEGRO_BITMAP *perso1 = NULL;
+ALLEGRO_BITMAP *perso2 = NULL;
+ALLEGRO_BITMAP *perso3 = NULL;
+ALLEGRO_BITMAP *perso4 = NULL;
+ALLEGRO_BITMAP *perso5 = NULL;
+ALLEGRO_BITMAP *perso6 = NULL;
+/* personagens */
+
 ALLEGRO_TIMER *timer = NULL; // inicia o timer
 void fadeout(int velocidade);  //função pra dar o fadeout
 void checavalidespos(int posx,int posy,int passo,int* or);
@@ -29,7 +45,9 @@ int main(void){
     int desenha = 1; //inicio desenha como 1
     int posx = 90, dir_x = passo; //seto a posição e a passada do personagem
     int posy = 270, dir_y = passo;
-
+    int jogar = 1;
+    int selecao;
+    
     /* variaveis da animacao do personagem */
  	const int maxFrame = 2;
  	int curFrame = 0;
@@ -45,12 +63,111 @@ int main(void){
     al_set_audio_stream_playing(musica, true); //comeca a musica
 
     fadein(fundo, 1); //a imagem do pano de fundo entra
-    al_rest(3.0); //dica durante 3 segundos
+    al_rest(tempofade); //dica durante 3 segundos
     fadeout(1);//e sai
     grupo = al_load_bitmap("resources/group.jpeg"); //seta a imagem do grupo
     fadein(grupo, 1); //faz a aparecer a imagem do grupo
-    al_rest(3.0); //€spera 3 segundos
+    al_rest(tempofade); //€spera 3 segundos
     fadeout(1); //e sai
+    menu = al_load_bitmap("resources/menu.bmp");
+    fadein(menu, 1);
+
+    while(jogar != 0){
+        al_draw_bitmap(menu, 0, 0, 0);
+            while(!al_is_event_queue_empty(fila_eventos)){
+                ALLEGRO_EVENT evento;
+                al_wait_for_event(fila_eventos, &evento);
+                
+                if(evento.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP){
+                    /* botao de inicio */
+                    if(evento.mouse.x >= 112 && evento.mouse.x <= 362 &&
+                    evento.mouse.y >= 482 && evento.mouse.y <= 580){
+                        jogar = 0;
+                        selecao = 1;
+                    }
+                    /* botao de opcoes */
+                    if(evento.mouse.x >= 382 && evento.mouse.x <= 607 &&
+                    evento.mouse.y >= 482 && evento.mouse.y <= 580){
+                        jogar = 0;
+                    }
+                    /* botao de sair */
+                    if(evento.mouse.x >= 626 && evento.mouse.x <= 862 &&
+                    evento.mouse.y >= 482 && evento.mouse.y <= 580){
+                        return 0;
+                    }
+                }
+            }
+        al_rest(0.1);
+    }
+
+    fadeout(1);
+    personagens = al_load_bitmap("resources/perso.png");
+    fadein(personagens, 1);
+    
+    perso1 = al_load_bitmap("resources/perso1.png");
+    perso2 = al_load_bitmap("resources/perso2.png");
+    perso3 = al_load_bitmap("resources/perso3.png");
+    perso4 = al_load_bitmap("resources/perso4.png");
+    perso5 = al_load_bitmap("resources/perso5.png");
+    perso6 = al_load_bitmap("resources/perso6.png");
+    
+    while(selecao != 0){
+        al_draw_bitmap(personagens, 0, 0, 0);
+        
+        al_convert_mask_to_alpha(perso1,al_map_rgb(255,0,255));
+        al_convert_mask_to_alpha(perso2,al_map_rgb(255,0,255));
+        al_convert_mask_to_alpha(perso3,al_map_rgb(255,0,255));
+        al_convert_mask_to_alpha(perso4,al_map_rgb(255,0,255));
+        al_convert_mask_to_alpha(perso5,al_map_rgb(255,0,255));
+        al_convert_mask_to_alpha(perso6,al_map_rgb(255,0,255));
+        al_draw_bitmap(perso1, 157, 339, 0);
+        al_draw_bitmap(perso2, 422, 341, 0);
+        al_draw_bitmap(perso3, 709, 342, 0);
+        al_draw_bitmap(perso4, 151, 551, 0);
+        al_draw_bitmap(perso5, 420, 552, 0);
+        al_draw_bitmap(perso6, 705, 552, 0);
+        al_flip_display();
+        
+            while(!al_is_event_queue_empty(fila_eventos)){
+                ALLEGRO_EVENT evento;
+                al_wait_for_event(fila_eventos, &evento);
+                
+                if(evento.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP){
+                    if(evento.mouse.x >= 157 && evento.mouse.x <= 339 &&
+                    evento.mouse.y >= 339 && evento.mouse.y <= 432){
+                        fprintf(stderr, "PERSO1\n");
+                        selecao = 0;
+                    }
+                    if(evento.mouse.x >= 422 && evento.mouse.x <= 519 &&
+                    evento.mouse.y >= 341 && evento.mouse.y <= 434){
+                        fprintf(stderr, "PERSO2\n");
+                        selecao = 0;
+                    }
+                    if(evento.mouse.x >= 709 && evento.mouse.x <= 807 &&
+                    evento.mouse.y >= 342 && evento.mouse.y <= 435){
+                        fprintf(stderr, "PERSO3\n");
+                        selecao = 0;
+                    }
+                    if(evento.mouse.x >= 151 && evento.mouse.x <= 246 &&
+                    evento.mouse.y >= 551 && evento.mouse.y <= 644){
+                        fprintf(stderr, "PERSO4\n");
+                        selecao = 0;
+                    }
+                    if(evento.mouse.x >= 420 && evento.mouse.x <= 515 &&
+                    evento.mouse.y >= 552 && evento.mouse.y <= 645){
+                        fprintf(stderr, "PERSO5\n");
+                        selecao = 0;
+                    }
+                    if(evento.mouse.x >= 705 && evento.mouse.x <= 801 &&
+                    evento.mouse.y >= 552 && evento.mouse.y <= 645){
+                        fprintf(stderr, "PERSO6\n");
+                        selecao = 0;
+                    }
+                }
+            }
+        al_rest(0.1);
+    }                    
+    
     imagem = al_load_bitmap("resources/aa.png");  //faz o download do mapa do jogo
     fadein(imagem, 1); //faz ela aparecer
 	al_draw_bitmap(imagem, 0, 0, 0);  //coloca a imagem
@@ -62,11 +179,10 @@ int main(void){
         while (!al_is_event_queue_empty(fila_eventos)){//se  acontecer algo
             ALLEGRO_EVENT evento;   //declara variavel eveno
             al_wait_for_event(fila_eventos, &evento);//esse evento fica na fila
-
+            
 		        if(evento.type == ALLEGRO_EVENT_TIMER){
 		        	// aqui vai ser o timer (pretendo usar pra controlar o 'loop' da musica)
 		        }
-
 
                  if (evento.keyboard.keycode == ALLEGRO_KEY_W){ //ægora ele checa se tem colisao, para cima
 			        posy -= dir_y;
@@ -95,6 +211,7 @@ int main(void){
                 }
             if (evento.type == ALLEGRO_EVENT_DISPLAY_CLOSE){  //se n tiver evento, sai 
                 sair = true;
+                return 0;
             }
             if(desenha && al_is_event_queue_empty(fila_eventos)) {
 	            if(tecl == 'S' || tecl == 'W' || tecl == 'A' || tecl == 'D'){
@@ -170,6 +287,7 @@ void fadein(ALLEGRO_BITMAP *imagem, int velocidade)
 }
 bool inicializar(){ //inicializa tudo e checa se tudo deu crto
 	al_init_image_addon();
+    
     if (!al_init()){
         fprintf(stderr, "Falha ao inicializar Allegro.\n");
         return false;
@@ -199,6 +317,21 @@ bool inicializar(){ //inicializa tudo e checa se tudo deu crto
         fprintf(stderr, "Falha ao criar a janela.\n");
         return false;
     }
+    
+	// inicialização e configuração do mouse
+	if(!al_install_mouse()){
+	    fprintf(stderr, "Falha ao inicializar o mouse");
+	    al_destroy_display(janela);
+	    return -1;
+	}
+	// Atribui o cursor padrão do sistema para ser usado
+    if (!al_set_system_mouse_cursor(janela, ALLEGRO_SYSTEM_MOUSE_CURSOR_DEFAULT))
+    {
+        fprintf(stderr, "Falha ao atribuir ponteiro do mouse.\n");
+        al_destroy_display(janela);
+        return -1;
+    }
+    
     fila_eventos = al_create_event_queue();
     if (!fila_eventos){
         fprintf(stderr, "Falha ao criar fila de eventos.\n");
@@ -228,7 +361,7 @@ bool inicializar(){ //inicializa tudo e checa se tudo deu crto
     fundo = al_load_bitmap("resources/cin.jpeg");
     al_register_event_source(fila_eventos, al_get_keyboard_event_source());
     al_register_event_source(fila_eventos, al_get_display_event_source(janela));
-
+    al_register_event_source(fila_eventos, al_get_mouse_event_source());
    	al_register_event_source(fila_eventos, al_get_timer_event_source(timer));
     al_start_timer(timer);
     return true;
@@ -240,6 +373,7 @@ void setAudio(char k[]){ //comeca musiquinha
  	al_attach_audio_stream_to_mixer(musica, al_get_default_mixer());
  	al_set_audio_stream_playing(musica, true);
 }
+
 void checavalidespos(int posx,int posy,int passo,int* or){
     if(posx>=0&&posx<=960&&posy>=0&&posy<=70){
         *or-=passo;
