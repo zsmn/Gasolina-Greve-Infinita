904e0d61fd88f9022ae005dcd9a52fe0a8fdd492
danperazzo <39570908+danperazzo@users.noreply.github.com>
2018-06-07 16:11:32 -0300

    Update serverprot.c
    servidor

--- IPGame/serverprot.c
+++ IPGame/serverprot.c
@@ -2,22 +2,23 @@
 #include "server.h"
 #include <stdio.h>
 #include <string.h>
-#define jogadores 2
+#define jogadores 4
 #define mensagem struct msg_ret_t
 void checavalidespos(unsigned short posx,unsigned short posy,unsigned short passo,unsigned short* or);
 int main() {
-  int  pos[jogadores][2];
-  int aux[2];
-  pos[0][0]=8;
-  pos[0][1]=17;
-  pos[1][0]=8;
-  pos[1][1]=17;
-  serverInit(2);
+  char  pos[jogadores][2];
+  char aux[2];
+  int i;
+  for(i=0;i<jogadores;i++){
+   pos[i][0]=8;
+   pos[i][1]=17;
+  }
+  serverInit(jogadores);
   char tecla;
   int old;
   char dif;
   puts("o servidor esta funcionando!!");
-  int count=0,i;
+  int count=0;
   int id;
   while(count<jogadores){
     id = acceptConnection();
@@ -33,9 +34,7 @@ int main() {
       pos[msgjog.client_id][0]=aux[0];
       pos[msgjog.client_id][1]=aux[1];
       //broadcast(pos,2*sizeof(int));
-      for(i=0;i<jogadores;i++){
-         fprintf(stderr," jogadorID: %d posicao x:%d posicao y:%d\n",i,pos[i][0],pos[i][1]);
-      }    
+      broadcast(pos,jogadores*2*sizeof(char)); 
     } 
   }
 }
